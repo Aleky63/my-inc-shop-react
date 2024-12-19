542c2c0ad4c70a9b93e28c4c0d23f504e3c9c7b5
Aleksey <134486666+Aleky63@users.noreply.github.com>
2024-12-19 13:26:55 +0300

    01_19.12

--- shop/src/App.tsx
+++ shop/src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Route, Routes } from "react-router";
+import { BestSellers } from "./BestSellers";
+import { Header } from "./Header";
+import { Product } from "./Product";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="appContainer">
+      <Header />
+      <Routes>
+        <Route path="/" element={<BestSellers />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ shop/src/BestSellers.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+export type ProductType = {
+  _id: string;
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
+export const BestSellers = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // обращение к серверу при загрузке страницы
+    const promise = axios.get(
+      "https://masterclass.kimitsu.it-incubator.io/api/products"
+    );
+    promise.then((res) => {
+      const products = res.data;
+      setProducts(products);
+    });
+  }, []);
+
+  return (
+    <div className="bestSeller">
+      <h2>Bestsellers</h2>
+      <div className="cards">
+        {products.map((pr: { id: any; image: any; title: any; price: any }) => {
+          return (
+            <div className="card">
+              <img src={pr.image} alt="img" />
+              <h4>{pr.title}</h4>
+              <p className="price">${pr.price}</p>
+              <Link to={`/product/${pr.id}`}>Show more</Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
